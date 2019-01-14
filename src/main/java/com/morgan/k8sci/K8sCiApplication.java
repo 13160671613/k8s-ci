2bf84b60251fe69a2e1781f44cd7102ad28ec0be
13160671613 <13160671613@163.com>
2019-01-15 00:27:20 +0800

    init

---
+++ src/main/java/com/morgan/k8sci/K8sCiApplication.java
@@ -0,0 +1,26 @@
+package com.morgan.k8sci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class K8sCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sCiApplication.class, args);
+    }
+
+    @GetMapping("/k8s")
+    public String k8s(){
+        return "k8s";
+    }
+
+    @GetMapping(value = "/hello")
+    public String hello() {
+        return "hello1";
+    }
+}
+
